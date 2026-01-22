bfb957b95942eceadb0847046fe4b6bb195c7b81
ErikKalikas <231414480+ErikKalikas@users.noreply.github.com>
2026-01-22 14:14:47 +0200

    upd1

--- IF_loogilisedoperaatorid_tingimus/IF-ELSEIF-ELSE/Program.cs
+++ IF_loogilisedoperaatorid_tingimus/IF-ELSEIF-ELSE/Program.cs
@@ -41,97 +41,97 @@ namespace IF_ELSEIF_ELSE
             //  kui 2FA kood on 6 tähte, siis lase sisse, öeldes "oled sissepääsenud"
             //NB: 2FA kood ei pea matchima eksisteeriva näitega nagu parool.
 
-            Console.WriteLine("kirjuta oma pikkust");
-            int pikkust = Convert.ToInt32 (Console.ReadLine());
-            if (pikkust <= 0)
+            Console.WriteLine("kirjuta oma pikkust"); //väljastab teksti: "kirjuta oma pikkust"
+            int pikkust = Convert.ToInt32 (Console.ReadLine()); //luuakse muutuja tüübiga int, mis teisendab Convert.ToInt32 abil sisestatud vastuse Console.ReadLine()-st.      
+            if (pikkust <= 0) //Kas „pikust“ on väiksem või võrdne 0-ga?
             {
-                Console.WriteLine("võimatu pikkus");
+                Console.WriteLine("võimatu pikkus"); //väljastab teksti: "võimatu pikkus"
             }
-            else if (pikkust == 1)
+            else if (pikkust == 1) //Kas „pikust“ on võrdne 1 - ga ?
             {
-                Console.WriteLine("juntsu");
+                Console.WriteLine("juntsu"); //väljastab teksti: "juntsu"
             }
-            else if (pikkust == 2)
+            else if (pikkust == 2) //Kas „pikust“ on võrdne 2 - ga ?
             {
-                Console.WriteLine("tavaline kolge");
+                Console.WriteLine("tavaline kolge"); //väljastab teksti: "tavaline kolge"
             }
-            else if (pikkust > 2)
+            else if (pikkust > 2) //Kas „pikust“ on rohkem kui 2 ?
             {
-                Console.WriteLine("hiiglane");
+                Console.WriteLine("hiiglane"); //väljastab teksti: "hiiglane"
             }
-            Console.WriteLine("kui vana sa oled");
-            int vanus = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("kirjuta oma aastaarvu");
-            int aeg = Convert.ToInt32(Console.ReadLine());
-            int kod = aeg - vanus;
-            if (kod >= 2020 && kod <= 2025)
+            Console.WriteLine("kui vana sa oled"); //väljastab teksti: "kui vana sa oled"
+            int vanus = Convert.ToInt32(Console.ReadLine()); //luuakse muutuja tüübiga int "vanus", mis teisendab Convert.ToInt32 abil sisestatud vastuse Console.ReadLine() - st.           
+            Console.WriteLine("kirjuta oma aastaarvu"); //väljastab teksti: "kirjuta oma aastaarvu"
+            int aeg = Convert.ToInt32(Console.ReadLine()); //luuakse muutuja tüübiga int "aeg", mis teisendab Convert.ToInt32 abil sisestatud vastuse Console.ReadLine() - st.  
+            int kod = aeg - vanus; //luuakse muutuja kood, milles salvestatakse arvutuse aeg - vanus tulemus;
+            if (kod >= 2020 && kod <= 2025) //Kas „kod“ on suurem või võrdne 2015-ga? Ja kas „kod“ on väiksem või võrdne 2025 - ga ?             
             {
-                Console.WriteLine(" kes sulle arvuti juba andis");
+                Console.WriteLine(" kes sulle arvuti juba andis"); //väljastab teksti: " kes sulle arvuti juba andis"
             }
-            else if (kod >= 2015 && kod <= 2019)
+            else if (kod >= 2015 && kod <= 2019) //Kas „kod“ on suurem või võrdne 2015-ga? Ja kas „kod“ on väiksem või võrdne 2019 - ga ?                
             {
-                Console.WriteLine("skibidi sigmaless");
+                Console.WriteLine("skibidi sigmaless"); //väljastab teksti: "skibidi sigmaless"
             }
-            else if (kod <= 2015 && kod >= 2010)
+            else if (kod <= 2015 && kod >= 2010) //Kas „kod“ on väiksem või võrdne 2015-ga? Ja kas „kod“ on suurem või võrdne 2010-ga?
             {                
-                while (true)
+                while (true) //sisenemine tsüklisse while. tingimused väljumiseks „true“
                 {
-                    Console.WriteLine("kas ta tahab saada programmeerijaks (JAH/EI)");
-                    string tahab = Console.ReadLine();
-                    if (tahab == "JAH")
+                    Console.WriteLine("kas ta tahab saada programmeerijaks (JAH/EI)"); //väljastab teksti: "kas ta tahab saada programmeerijaks (JAH/EI)"
+                    string tahab = Console.ReadLine(); //luuakse string-tüüpi muutuja "tahab", kuhu kirjutatakse vastus Console.ReadLine();
+                    if (tahab == "JAH") //Kas „tahab“ on võrdne sõnaga „JAH“?
                     {
-                        Console.WriteLine("tubli");
-                        break;
+                        Console.WriteLine("tubli"); //väljastab teksti: "tubli"
+                        break; //tsükli väljumine
                     }
-                    else if (tahab == "EI")
+                    else if (tahab == "EI") ////Kas „tahab“ on võrdne sõnaga „JAH“?
                     {
-                        Console.WriteLine("kahju");
-                        break;
+                        Console.WriteLine("kahju"); //väljastab teksti: "kahju"
+                        break; //tsükli väljumine
                     }
                     
                 }
             }
             while (true)
             {
-                Console.WriteLine("kuidas sinu suunakood?");
-                int suunakood = Convert.ToInt32(Console.ReadLine());
-                if (suunakood <= 9999)
+                Console.WriteLine("kuidas sinu suunakood?"); //väljastab teksti: "kuidas sinu suunakood?"
+                int suunakood = Convert.ToInt32(Console.ReadLine()); //luuakse muutuja tüübiga int "suunakood", mis teisendab Convert.ToInt32 abil sisestatud vastuse Console.ReadLine() - st. 
+                if (suunakood <= 9999) //Kas „suunakood“ on väiksem või võrdne 9999-ga?
                 {
-                    Console.WriteLine("1 täht on puudu");
-                    
+                    Console.WriteLine("1 täht on puudu"); //väljastab teksti: "1 täht on puudu"
+
                 }
-                else if (suunakood >= 1000000)
+                else if (suunakood >= 1000000) //Kas „suunakood“ on suurem või võrdne 1000000-ga?
                 {
-                    Console.WriteLine("1 täht on üle");
-                    
+                    Console.WriteLine("1 täht on üle"); //väljastab teksti: "1 täht on üle"
+
                 }
                 else
                 {
-                    Console.WriteLine("ahha nüüd tean kus elad");
-                    break;
+                    Console.WriteLine("ahha nüüd tean kus elad"); //väljastab teksti: "ahha nüüd tean kus elad"
+                    break; //tsükli väljumine
                 }
             }
 
             while (true)
             {
-                Console.WriteLine("kas ta töötab? (JAH/EI)");
-                string töö = Console.ReadLine();
-                if (töö == "JAH")
+                Console.WriteLine("kas ta töötab? (JAH/EI)"); //väljastab teksti: "kas ta töötab? (JAH/EI)"
+                string töö = Console.ReadLine(); //luuakse string-tüüpi muutuja "töö", kuhu kirjutatakse vastus Console.ReadLine();
+                if (töö == "JAH")  //Kas „tahab“ on võrdne sõnaga „JAH“?
                 {
-                    Console.WriteLine("tubli");
-                    break;
+                    Console.WriteLine("tubli"); //väljastab teksti: "tubli"
+                    break; //tsükli väljumine
                 }
-                else if (töö == "EI")
+                else if (töö == "EI") //Kas „tahab“ on võrdne sõnaga „EI“?
                 {
-                    Console.WriteLine("kahju");
-                    Console.WriteLine("miks sa siis töökohta ei otsi?");
+                    Console.WriteLine("kahju"); //väljastab teksti: "kahju"
+                    Console.WriteLine("miks sa siis töökohta ei otsi?"); //väljastab teksti: "miks sa siis töökohta ei otsi?"
                     string otsin = Console.ReadLine();
-                    if (otsin == "otsin")
+                    if (otsin == "otsin")  //Kas „tahab“ on võrdne sõnaga „otsin“?
                     {
-                        Console.WriteLine("loodan et saad varsti tööle c:");
-                        
+                        Console.WriteLine("loodan et saad varsti tööle c:"); //väljastab teksti: "loodan et saad varsti tööle c:"
+
                     }                               
-                        break;      
+                        break;      //tsükli väljumine
                 }
                 
             }           
@@ -139,33 +139,35 @@ namespace IF_ELSEIF_ELSE
                        
             while (true)
             {
-                Console.WriteLine("kirjuta oma parool");
-                string parool = Console.ReadLine();
-                if (parool == "parool")
+                Console.WriteLine("kirjuta oma parool"); //väljastab teksti: "kirjuta oma parool"
+                string parool = Console.ReadLine(); //luuakse string-tüüpi muutuja "parool", kuhu kirjutatakse vastus Console.ReadLine();
+                if (parool == "parool") 
                 {
-                    Console.WriteLine("kirjuta oma 2FA koodi");
+                    Console.WriteLine("kirjuta oma 2FA koodi"); //väljastab teksti: "kirjuta oma 2FA koodi"
                     int kood = Convert.ToInt32(Console.ReadLine());
-                    if (kood >= 999999)
+                    if (kood >= 999999) 
                     {
-                        Console.WriteLine("kood on liiga pikk");
-                        
+                        Console.WriteLine("kood on liiga pikk"); //väljastab teksti: "kood on liiga pikk"
+
                     }
                     else if (kood <= 99999)
                     {
-                        Console.WriteLine("kood on liiga lühike");
-                        
+                        Console.WriteLine("kood on liiga lühike"); //väljastab teksti: "kood on liiga lühike"
+
                     }
                     else
                     {
-                        Console.WriteLine("oled sissepääsenud");
+                        Console.WriteLine("oled sissepääsenud"); //väljastab teksti: "oled sissepääsenud"
                         break;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("sissepääs keelatud");
-                    
+                    Console.WriteLine("sissepääs keelatud"); //väljastab teksti: "sissepääs keelatud"
+
                 }
+
+                
             }
                
         }
